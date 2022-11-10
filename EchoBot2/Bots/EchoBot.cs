921dad7bef23746b4a659237a08babe0c93d78ad
Enrique Candil Botello <ecandil99@gmail.com>
2022-11-10 14:30:39 +0100

    Update EchoBot.cs

--- EchoBot2/Bots/EchoBot.cs
+++ EchoBot2/Bots/EchoBot.cs
@@ -21,7 +21,7 @@ namespace EchoBot.Bots
 
         protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
         {
-            var welcomeText = "Hello and welcome!";
+            var welcomeText = "Hello";
             foreach (var member in membersAdded)
             {
                 if (member.Id != turnContext.Activity.Recipient.Id)
