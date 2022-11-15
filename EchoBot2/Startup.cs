f1b65545b40ad47f00fcc135f066d4a1a6e4923b
Enrique Candil Botello <ecandil99@gmail.com>
2022-11-15 18:29:39 +0100

    Update Startup.cs

--- EchoBot2/Startup.cs
+++ EchoBot2/Startup.cs
@@ -47,6 +47,8 @@ namespace EchoBot
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+            }else{
+                app.UseHsts();
             }
 
             app.UseDefaultFiles()
