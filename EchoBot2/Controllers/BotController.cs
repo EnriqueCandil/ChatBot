849c1a110f25e033980d8e98dd83d6787eab7aed
Enrique Candil Botello <ecandil99@gmail.com>
2022-12-16 15:52:39 +0100

    a

--- EchoBot2/Controllers/BotController.cs
+++ EchoBot2/Controllers/BotController.cs
@@ -6,6 +6,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using System;
 using System.Threading.Tasks;
 
 namespace EchoBot.Controllers
@@ -32,7 +33,13 @@ namespace EchoBot.Controllers
         {
             // Delegate the processing of the HTTP POST to the adapter.
             // The adapter will invoke the bot.
-            await _adapter.ProcessAsync(Request, Response, _bot);
+            try
+            {
+                await _adapter.ProcessAsync(Request, Response, _bot);
+            }
+            catch(Exception e) { 
+                Console.WriteLine(e); 
+            }
         }
     }
 }
